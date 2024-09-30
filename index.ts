80804916b0f9b1b4c396218323ef676a24340c99
FuzzilFox1424 <125853450+CN-LEON-DX@users.noreply.github.com>
2024-09-30 14:40:33 +0700

    basic route

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
+import express, { Express, Request, Response } from "express";
 
+const app: Express = express();
+const port: string | number = process.env.PORT || 3000;
 
-console.log('Hello World!');
+app.use("/topics", (req: Request, res: Response) => {
+  res.send("List topic");
+});
+
+app.listen(port, () => {
+  console.log("App listening at port " + port);
+});
